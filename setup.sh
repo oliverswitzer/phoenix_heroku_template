76cb07d43ff301995456589044dbad742567083a
Oliver Switzer <oliver@geometer.io>
2022-01-08 03:49:53 -0500

    Add Heroku setup to setup.sh script

--- setup.sh
+++ setup.sh
@@ -31,7 +31,23 @@ else
   exit 1
 fi
 
-echo "What module name would you like to give this app? \n\nExample: MyApp would create MyApp and MyAppWeb module prefixes"
+cat << EOF
+
+
+What module name would you like to give this app? 
+
+  Example: MyApp would create MyApp and MyAppWeb module prefixes, with a file
+    structure like so:
+
+      * lib/my_app/...
+      * lib/my_app_web/..
+      * test/my_app/...
+      * test/my_app_web/...
+
+      Note: it will also be used as the default name for your Heroku app when automatically deploying to Heroku later, ie my-app.herokuapp.com (you can skip this step if you'd like)
+
+EOF
+
 read module_name
 
 snake_name=$(echo $module_name | sed 's/[[:upper:]]/_&/g;s/^_//' | tr '[:upper:]' '[:lower:]')
@@ -51,25 +67,52 @@ find . -execdir rename -f 's/rename_me/\Q$ENV{snake_name}\E/' '{}' \+ &> /dev/nu
 echo "Heads up! Also renaming the current working directory to $snake_name..."
 rename_cwd $snake_name
 
-cat << EOF
+echo "✅ Done! your app is renamed to $snake_name / $module_name."
+echo 
+                                                                             
+read -p "Do you want to setup Heroku? Y/n " -n 1 -r
+
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then 
+  echo "First compiling the app to be able run mix phx.gen.secret..."
+  mix deps.get && mix compile 
+
+  kebab_name=$(echo $snake_name | awk '{ gsub("_", "-") ; system( "echo "  $0) }')
+
+  echo "Creating Heroku app with Postgresql Hobby Dev database..."
+  heroku create $kebab_name
+  heroku addons:create heroku-postgresql:hobby-dev
+  heroku buildpacks:add hashnuke/elixir
+  heroku config:set POOL_SIZE=10 SECRET_KEY_BASE=$(mix phx.gen.secret) PHX_HOST=$(heroku domains | awk 'FNR == 2 {print}')
+  git push heroku main
+
+  cat << EOF
  ______     __  __     ______     ______     ______     ______     ______    
 /\  ___\   /\ \/\ \   /\  ___\   /\  ___\   /\  ___\   /\  ___\   /\  ___\   
 \ \___  \  \ \ \_\ \  \ \ \____  \ \ \____  \ \  __\   \ \___  \  \ \___  \  
  \/\_____\  \ \_____\  \ \_____\  \ \_____\  \ \_____\  \/\_____\  \/\_____\ 
   \/_____/   \/_____/   \/_____/   \/_____/   \/_____/   \/_____/   \/_____/ 
 EOF
-echo 
-echo "✅ Done! your app is renamed to $snake_name / $module_name."
-echo 
-                                                                             
-read -p "Do you want to compile & run the app? (ensure that you have postgres running
-first) Y/n " -n 1 -r
+  echo 
+  echo "🐿  Reminder don't forget to setup continuous deployment! Open
+  .github/workflows/main.yml and plug in your Heroku details to setup
+  auto-deploys to Heroku"
+  sleep 5
+  heroku open
+else
+  echo "Skipping: deploy to Heroku"
+fi
+
+echo
+echo
+read -p "Do you want to run the app locally? (ensure that you have postgres running first) Y/n " -n 1 -r
 
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  mix deps.get && mix ecto.create && mix ecto.migrate && mix phx.server
+  mix ecto.create && mix ecto.migrate && mix phx.server
 else
-  echo "Aborting, not attempting to run the app"
-  exit 1
+  echo "Skipping: compile & run the app"
 fi
+
